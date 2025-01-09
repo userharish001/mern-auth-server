ab21e3ec278bf1b930661a70e8dcde1eb6ff0d52
HarishSharma001 <harish9499428306@gmail.com>
2025-01-09 12:44:56 +0530

    minor

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 connectDB();
 
 
-const allowedOrigins = ['https://mern-auth-client-j8z6.onrender.com'];
+const allowedOrigins = ['http://localhost:5173'];
 
 app.use(express.json());
 app.use(cookieParser());
